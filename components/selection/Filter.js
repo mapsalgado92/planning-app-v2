ea79d85624705e9f0d98bea09c6413edfc43089b
Mario Salgado <mapsalgado92@gmail.com>
2022-04-02 19:16:36 +0100

    Added data init

---
+++ components/selection/Filter.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+/**
+ *
+ * @param {
+ * items: Object []
+ * styles: Object
+ * } param0
+ * @returns
+ *
+ * item: {
+ *      name: String,
+ *      payload: Object
+ * }
+ *
+ * styles: {
+ *      button: String (className),
+ *      display: String (className),
+ * }
+ */
+
+const Filter = ({ items, styles }) => {
+  const [selected, setSelected] = useState([])
+
+  return <div></div>
+}
+
+export default Filter
