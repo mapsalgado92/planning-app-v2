4b08ef87656fc10d5d0d52025b6ebbb9f484d3fd
Mário Salgado <Mario.Salgado@sitel.com>
2022-04-06 15:37:54 +0100

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -8,28 +8,28 @@ import { AuthProvider } from "../contexts/authContext"
 import PageLayout from "../layout/pageLayout"
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name="description" content="Planning App" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+	return (
+		<>
+			<Head>
+				<meta name="description" content="Planning App" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/static/favicon-32x32.png" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
 
-        <link
-          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;600;700&display=swap"
+					rel="stylesheet"
+				></link>
+			</Head>
 
-      <AuthProvider>
-        <PageLayout>
-          <Component {...pageProps} />
-        </PageLayout>
-      </AuthProvider>
-    </>
-  )
+			<AuthProvider>
+				<PageLayout>
+					<Component {...pageProps} />
+				</PageLayout>
+			</AuthProvider>
+		</>
+	)
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 import Head from "next/head"
 
-
 export default function Home({ isConnected }) {
-  return (
-    <>
-      <Head>
-        <title>Planning App | Home</title>
-      </Head>
-      <div className="mt-auto mb-auto">
-        <div className="columns">
-          <div className="column is-two-fifths has-text-centered mx-auto pb-6 pt-4">
-            <h1 className="is-size-1">{"<Planning App 2._/>"}</h1>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>Planning App | Home</title>
+			</Head>
+			<div className="mt-auto mb-auto">
+				<div className="columns">
+					<div className="column is-two-fifths has-text-centered mx-auto pb-6 pt-4">
+						<h1 className="is-size-1">{"<Planning App 2._/>"}</h1>
+						<h3 className="is-danger">Unle$$ed</h3>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
-
