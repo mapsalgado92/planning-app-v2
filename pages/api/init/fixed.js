956968527d6850c4fbde466ed3f1272fbcd25407
Mário Salgado <Mario.Salgado@sitel.com>
2022-04-06 11:38:56 +0100

    fields init

---
+++ pages/api/init/fixed.js
@@ -0,0 +1,38 @@
+import fields from "../../../data-init/fields.json"
+import languages from "../../../data-init/languages.json"
+import weeks from "../../../data-init/weeks.json"
+import { connectToDatabase } from "../../../lib/mongodb"
+import { ObjectId } from "mongodb"
+
+export default async function handler(req, res) {
+	const { client, db } = await connectToDatabase()
+	console.log(client)
+
+	if (client) {
+		await db
+			.collection("fields")
+			.insertMany(
+				fields.map((item) => ({ ...item, _id: ObjectId(item._id["$oid"]) }))
+			)
+
+		await db
+			.collection("languages")
+			.insertMany(
+				languages.map((item) => ({ ...item, _id: ObjectId(item._id["$oid"]) }))
+			)
+
+		await db
+			.collection("weeks")
+			.insertMany(
+				weeks.map((item) => ({ ...item, _id: ObjectId(item._id["$oid"]) }))
+			)
+
+		res.status(200).json({
+			message: `Updated Entry in Database!`,
+			inserted: { fields, weeks, languages },
+		})
+	} else {
+		//BAD REQUEST
+		res.status(405).json({ message: "Method not Allowed, use POST only" })
+	}
+}
