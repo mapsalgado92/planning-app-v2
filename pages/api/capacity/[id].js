3deacd53f577ccdb69917d072e35be3346cc7090
Mário Salgado <Mario.Salgado@sitel.com>
2022-04-06 14:43:56 +0100

    fields init

--- pages/api/capacity/[id].js
+++ pages/api/capacity/[id].js
@@ -10,55 +10,61 @@ HEADER: authorization base 64 encoded
 */
 
 export default async function handler(req, res) {
-  const { query, method } = req
+	const { query, method } = req
 
-  const { client, db } = await connectToDatabase()
+	const { client, db } = await connectToDatabase()
 
-  let toWeek = query.to
-  let fromWeek = query.from
-  let id = query.id
+	let toWeek = query.to
+	let fromWeek = query.from
+	let id = query.id
 
-  switch (method) {
-    case "GET":
-      let capPlan = await db
-        .collection("capPlans")
-        .findOne({ _id: ObjectId(id) })
+	switch (method) {
+		case "GET":
+			let capPlan = await db
+				.collection("capPlans")
+				.findOne({ _id: ObjectId(id) })
 
-      if (!capPlan) {
-        res.status(404).json({ message: "Capacity Plan not Found!" })
-      }
-      let entries = await db
-        .collection("capEntries")
-        .find({ capPlan: id })
-        .toArray()
+			if (!capPlan) {
+				res.status(404).json({ message: "Capacity Plan not Found!" })
+			}
+			let entries = await db
+				.collection("capEntries")
+				.find({ capPlan: id })
+				.toArray()
 
-      let weeks = await db
-        .collection("weeks")
-        .find({})
-        .sort({ firstDate: 1 })
-        .toArray()
+			let weeks = await db
+				.collection("weeks")
+				.find({})
+				.sort({ firstDate: 1 })
+				.toArray()
 
-      if (weeks && toWeek) {
-        weeks = weeks.slice(
-          0,
-          1 + weeks.indexOf(weeks.find((week) => week.code === toWeek))
-        )
-      }
+			if (weeks && toWeek) {
+				weeks = weeks.slice(
+					0,
+					1 + weeks.indexOf(weeks.find((week) => week.code === toWeek))
+				)
+			}
 
-      console.log("CAP PLAN", capPlan)
+			console.log("Weeks", weeks)
 
-      let capacity = generateCapacity(capPlan, entries, weeks)
+			try {
+				let capacity = generateCapacity(capPlan, entries, weeks)
 
-      if (capacity && fromWeek) {
-        capacity = capacity.slice(
-          0 + weeks.indexOf(weeks.find((week) => week.code === fromWeek))
-        )
-      }
+				if (capacity && fromWeek) {
+					capacity = capacity.slice(
+						0 + weeks.indexOf(weeks.find((week) => week.code === fromWeek))
+					)
+				}
 
-      res.status(200).json({ message: "Capacity Generated", capacity })
+				res.status(200).json({ message: "Capacity Generated", capacity })
+			} catch (error) {
+				res
+					.status(500)
+					.json({ message: "Something went wrong", capPlan, weeks, entries })
+			}
 
-      break
-    default:
-      res.status(405).json({ message: "Method not Allowed, use GET only" })
-  }
+			break
+		default:
+			res.status(405).json({ message: "Method not Allowed, use GET only" })
+	}
 }
