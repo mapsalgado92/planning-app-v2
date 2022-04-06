b7115e0254ed7c06d134d294ef38ec4545c19e01
Mário Salgado <Mario.Salgado@sitel.com>
2022-04-06 11:23:36 +0100

    fields init

--- pages/api/init/fields.js
+++ pages/api/init/fields.js
@@ -1,17 +1,21 @@
 import fields from "../../../data-init/fields.json"
 import { connectToDatabase } from "../../../lib/mongodb"
+import { ObjectId } from "mongodb"
 
 export default async function handler(req, res) {
 	const { client, db } = await connectToDatabase()
 	console.log(client)
 
 	if (fields && client) {
-		let response = await db.collection("fields").insertMany(fields)
+		let response = await db
+			.collection("fields")
+			.insertMany(
+				fields.map((field) => ({ ...field, _id: ObjectId(field._id["$oid"]) }))
+			)
 
 		res.status(200).json({
 			message: `Updated Entry in Database!`,
 			inserted: fields,
-			response: response,
 		})
 	} else {
 		//BAD REQUEST
